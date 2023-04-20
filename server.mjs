90803dc0abe0b1776ddab94b691d15d04ba08a32
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:06:10 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,21 +1,6 @@
-const https = require('https');
-
-const options = {
-  method: 'GET',
-  hostname: 'api.brawlhalla.com',
-  path: '/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6',
-};
-
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
-
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-});
-
-req.on('error', (error) => {
-  console.error(error);
-});
-
-req.end();
+Http.Request("https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
