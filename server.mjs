5430c301c085fb86df2d06324fafd097b4bfb0c7
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:11:15 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,22 +1,30 @@
-import https from 'https';
+import http from 'http';
+import fetch from 'node-fetch';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
+const API_KEY = 'C2KZNXSHOPILAEPYOVH6';
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    try {
+      const response = await fetch(`https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=${API_KEY}`);
+      const data = await response.json();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(data));
+      res.end();
+    } catch (error) {
+      console.error(error);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.write('Internal Server Error');
+      res.end();
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Not Found');
+    res.end();
   }
-};
+});
 
-const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6';
-
-https.request(url, options, res => {
-  let data = '';
-
-  res.on('data', chunk => {
-    data += chunk;
-  });
-
-  res.on('end', () => {
-    console.log(JSON.parse(data));
-  });
-}).end();
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
