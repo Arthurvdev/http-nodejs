390ee2f499ef4139f4c98ea868a586c7416242f7
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:40:32 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,2 @@
-import { createServer } from 'http';
-
-createServer((req, res) => {
-  res.write(https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6);
-  res.end();
-}).listen(process.env.PORT);
+https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6);
+ 
