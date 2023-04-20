7ed9c87c57d08bde3f92e30ae09d467e4fa0985a
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:29:54 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,9 @@
 fetch('https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6')
-  .then(response => response.json())
-  .then(data => {
-    document.body.innerHTML = JSON.stringify(data);
-  })
-  .catch(error => console.error(error));
+      .then(response => response.json())
+      .then(data => {
+        const contentDiv = document.querySelector('#content');
+        contentDiv.textContent = JSON.stringify(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
