dae4f821ec9f71995fbcdae40a06e8373b280d23
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:26:47 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,26 +1,6 @@
-<script type="module">
-  import https from 'https';
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-
-  const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6';
-
-  https.request(url, options, res => {
-    let data = '';
-
-    res.on('data', chunk => {
-      data += chunk;
-    });
-
-    res.on('end', () => {
-      document.getElementById('data').innerHTML = JSON.stringify(JSON.parse(data));
-    });
-  }).end();
-</script>
-
-<div id="data"></div>
+fetch('https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6')
+  .then(response => response.json())
+  .then(data => {
+    document.body.innerHTML = JSON.stringify(data);
+  })
+  .catch(error => console.error(error));
