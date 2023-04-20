97688588dfb916df23cdb38ea71e6879fc1d7fc9
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:22:08 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,35 +1,26 @@
-import http from 'http';
-import https from 'https';
+<script type="module">
+  import https from 'https';
 
-const PORT = process.env.PORT || 3000;
-
-const requestHandler = (req, res) => {
   const options = {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     }
   };
-  
+
   const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6';
-  
-  https.request(url, options, apiResponse => {
+
+  https.request(url, options, res => {
     let data = '';
-  
-    apiResponse.on('data', chunk => {
+
+    res.on('data', chunk => {
       data += chunk;
     });
-  
-    apiResponse.on('end', () => {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(data);
-      res.end();
+
+    res.on('end', () => {
+      document.getElementById('data').innerHTML = JSON.stringify(JSON.parse(data));
     });
   }).end();
-};
+</script>
 
-const server = http.createServer(requestHandler);
-
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+<div id="data"></div>
