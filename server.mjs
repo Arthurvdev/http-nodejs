899eabaf5a87237cfe96c6e739f3cc2cb585b1bd
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:19:10 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,28 +1,35 @@
-import https from 'https';
 import http from 'http';
+import https from 'https';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
+const PORT = process.env.PORT || 3000;
 
-const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6';
-
-const server = http.createServer((req, res) => {
-  https.request(url, options, apiRes => {
+const requestHandler = (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  
+  const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6';
+  
+  https.request(url, options, apiResponse => {
     let data = '';
-    apiRes.on('data', chunk => {
+  
+    apiResponse.on('data', chunk => {
       data += chunk;
     });
-    apiRes.on('end', () => {
+  
+    apiResponse.on('end', () => {
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(data);
+      res.write(data);
+      res.end();
     });
   }).end();
-});
+};
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
