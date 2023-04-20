c9d6e0a8376b61feac7b496281c3c8180ac9e9b6
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-20 14:31:18 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,9 +1,20 @@
-fetch('https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6')
-      .then(response => response.json())
-      .then(data => {
-        const contentDiv = document.querySelector('#content');
-        contentDiv.textContent = JSON.stringify(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+import fetch from 'node-fetch';
+import express from 'express';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await fetch('https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6');
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
