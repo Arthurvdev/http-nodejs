65071a814c176d85517e1a1a5b70aa06045b9229
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-21 13:06:45 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -9,51 +9,21 @@ createServer((req, res) => {
     }
   };
 
-  const api_key = 'C2KZNXSHOPILAEPYOVH6';
-  const player_name = req.url.split('/')[2]; // obter o nome do jogador da URL da solicitação
+  const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6';
 
-  const urlPromises = []; // uma matriz de promessas de solicitação para cada página
-  for (let i = 1; i <= 10; i++) { // itera sobre as 10 páginas
-    const url = `https://api.brawlhalla.com/rankings/1v1/brz/${i}?api_key=${api_key}`;
-    urlPromises.push(new Promise((resolve, reject) => {
-      https.get(url, options, (apiRes) => {
-        let data = '';
+  https.get(url, options, (apiRes) => {
+    let data = '';
 
-        apiRes.on('data', (chunk) => {
-          data += chunk;
-        });
+    apiRes.on('data', (chunk) => {
+      data += chunk;
+    });
 
-        apiRes.on('end', () => {
-          const jsonData = JSON.parse(data);
-          resolve(jsonData);
-        });
-      }).on('error', (err) => {
-        reject(err);
-      });
-    }));
-  }
-
-  Promise.all(urlPromises)
-    .then((jsonArray) => {
-      const allData = jsonArray.reduce((accumulator, currentValue) => accumulator.concat(currentValue), []);
-      const playerData = allData.filter(row => row.name.toLowerCase() === player_name.toLowerCase());
-
+    apiRes.on('end', () => {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       res.writeHead(200, {'Content-Type': 'application/json'});
-
-      if (playerData.length > 0) {
-        res.write(JSON.stringify(playerData));
-      } else {
-        res.write(JSON.stringify({error: 'Jogador não encontrado'}));
-      }
-
-      res.end();
-    })
-    .catch(error => {
-      console.error(error);
-      res.writeHead(500, {'Content-Type': 'application/json'});
-      res.write(JSON.stringify({error: 'Erro ao buscar dados do jogador'}));
+      res.write(data);
       res.end();
     });
+  });
 }).listen(process.env.PORT);
